6303a65c2e44829bc0ddc8d8cb31342813d60b70
tjuic20-foi <tim.juic@student.foi.hr>
2023-04-25 14:22:23 +0200

    Add entity class that most game elements will inherit

---
+++ entity.js
@@ -0,0 +1,24 @@
+class Entity {
+   constructor(posX, posY, velX, velY, image) {
+      this.posX
+      this.posY
+      this.velX
+      this.velY
+      this.image
+   }
+}
+
+class RectangleEntity extends Entity {
+   constructor(posX, posY, width, height, velX, velY, image) {
+      super(posX, posY, velX, velY, image)
+      this.width = width;
+      this.height = height
+   }
+}
+
+class CircleEntity extends Entity {
+   constructor(posX, posY, radius, velX, velY, image) {
+      super(posX, posY, velX, velY, image)
+      this.radius = radius
+   }
+}
