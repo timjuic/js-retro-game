90e51829567acee89f64e349d4e45d469cf6c733
timjuic <tim.juic@gmail.com>
2023-04-27 08:49:24 +0200

    Draw method in entity class for other classes to inherit

--- entity.js
+++ entity.js
@@ -1,25 +1,34 @@
 class Entity {
-   constructor(posX, posY, velX, velY, image) {
+   constructor(posX, posY, velX, velY, color, image) {
       this.posX = posX
       this.posY = posY
       this.velX = velX
       this.velY = velY
+      this.color = color
       this.image = image
    }
 }
 
 class RectangleEntity extends Entity {
-   constructor(posX, posY, width, height, angle, velX, velY, image) {
-      super(posX, posY, velX, velY, image)
+   constructor(posX, posY, width, height, angle, velX, velY, color, image) {
+      super(posX, posY, velX, velY, color, image)
       this.width = width;
       this.height = height
       this.angle = angle
    }
+
+   draw() {
+      ctx.fillStyle = this.color
+      ctx.fillRect(0, 0, canvas.width, this.height)
+      ctx.fillRect(canvas.width - this.width, 0, this.width, canvas.height)
+      ctx.fillRect(0, canvas.height - this.height, canvas.width, this.height)
+      ctx.fillRect(0, 0, this.width, canvas.height)
+   }
 }
 
 class CircleEntity extends Entity {
-   constructor(posX, posY, radius, velX, velY, image) {
-      super(posX, posY, velX, velY, image)
+   constructor(posX, posY, radius, velX, velY, color, image) {
+      super(posX, posY, velX, velY, color, image)
       this.radius = radius
    }
 }
