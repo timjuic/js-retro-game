e586e6f12af1c3ff8780b6362a6aa1f5cb66a3d6
timjuic <tim.juic@gmail.com>
2023-04-27 10:09:11 +0200

    Fixed issues in entity class

--- entity.js
+++ entity.js
@@ -1,5 +1,6 @@
 class Entity {
-   constructor(posX, posY, velX, velY, color, image) {
+   constructor(game, posX, posY, velX, velY, color, image) {
+      this.game = game
       this.posX = posX
       this.posY = posY
       this.velX = velX
@@ -10,25 +11,25 @@ class Entity {
 }
 
 class RectangleEntity extends Entity {
-   constructor(posX, posY, width, height, angle, velX, velY, color, image) {
-      super(posX, posY, velX, velY, color, image)
+   constructor(game, posX, posY, width, height, angle, velX, velY, color, image) {
+      super(game, posX, posY, velX, velY, color, image)
       this.width = width;
       this.height = height
       this.angle = angle
    }
 
-   draw() {
+   draw(canvasName, player) {
+      console.log(player, this.posX, this.posY, this.width, this.height);
+      let canvas = this.game.getCanvasManager().getCanvas(canvasName)
+      let ctx = this.game.getCanvasManager().getContext(canvasName)
       ctx.fillStyle = this.color
-      ctx.fillRect(0, 0, canvas.width, this.height)
-      ctx.fillRect(canvas.width - this.width, 0, this.width, canvas.height)
-      ctx.fillRect(0, canvas.height - this.height, canvas.width, this.height)
-      ctx.fillRect(0, 0, this.width, canvas.height)
+      ctx.fillRect(this.posX, this.posY, this.posX + this.width, this.posY + this.height)
    }
 }
 
 class CircleEntity extends Entity {
-   constructor(posX, posY, radius, velX, velY, color, image) {
-      super(posX, posY, velX, velY, color, image)
+   constructor(game, posX, posY, radius, velX, velY, color, image) {
+      super(game, posX, posY, velX, velY, color, image)
       this.radius = radius
    }
 }
