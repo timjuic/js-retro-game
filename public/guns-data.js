430fadecad1f4bc4bf83e4fceab737b427459c2a
timjuic <tim.juic@gmail.com>
2023-08-21 18:25:58 +0200

    Redesigned the start page

--- public/guns-data.js
+++ public/guns-data.js
@@ -2,6 +2,6 @@ import Gun from "./gun.js";
 
 export default [
     new Gun("Shotgun", 100, 10, true, 5, 10, 200, false, 10, './assets/bullet.png'),
-    new Gun("Assault", 120, 30, true, 20, 1, 1, false, 10, './assets/bullet.png'),
+    new Gun("Assault", 80, 30, true, 20, 1, 50, false, 10, './assets/bullet.png'),
     new Gun("Sniper", 1000, 10, false, 100, 1, 1, true, 5, './assets/bullet.png')
 ]
